b19ed10d96920fc8f448f5f639e56de8a7605357
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-10 18:02:03 +0530

    Resizing event and sphere

--- SomeEdGuy.js
+++ SomeEdGuy.js
@@ -1,15 +1,36 @@
 import * as THREE from "three";
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+import "./style.css";
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const sizes = {
+  height: window.innerHeight,
+  width: window.innerWidth,
+};
+//scene
+const scene = new THREE.Scene();
+//camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(0, 0, 20);
+camera.lookAt(0, 0, 0);
+//renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+//light
+const light = new THREE.PointLight(0xffffff, 50, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+//sphere
+const sphereGeometery = new THREE.SphereGeometry(3, 64, 64);
+const sphereMaterial = new THREE.MeshStandardMaterial({ color: "#00ff83" });
+const sphereMesh = new THREE.Mesh(sphereGeometery, sphereMaterial);
+scene.add(sphereMesh);
 
 function animate() {
   requestAnimationFrame(animate);
@@ -17,3 +38,9 @@ function animate() {
 }
 
 animate();
+
+//resize
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+});
