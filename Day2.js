7ca09519dda360dedfd22fbd58fba137ec6cb546
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-10 10:33:18 +0530

    Day 2 experimenting

--- Day2.js
+++ Day2.js
@@ -1,32 +1,48 @@
 import * as THREE from "three";
+// must haves: scnene, camera, renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75, //field of view
+  45, //field of view
   window.innerWidth / window.innerHeight, //aspect ratio
   0.1, //near clippping
-  1000 //far clipping
+  500 //far clipping
 );
+camera.position.set(0, 0, 50);
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(5, 5, 5);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 //Torus
-const Pyramid_geometry = new THREE.TorusGeometry(1, 0.05);
+const Pyramid_geometry = new THREE.TorusGeometry(5, 0.25);
 const Pyramid_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
 const torus3 = new THREE.Mesh(Pyramid_geometry, Pyramid_material);
 const torus4 = new THREE.Mesh(Pyramid_geometry, Pyramid_material);
 scene.add(torus3);
 scene.add(torus4);
 
+//Lines
+const lineMaterial = new THREE.LineBasicMaterial({ color: "#ff0000" });
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+points.push(new THREE.Vector3(0, -10, 0));
+points.push(new THREE.Vector3(-10, 0, 0));
+
+const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(lineGeometry, lineMaterial);
+scene.add(line);
+
 torus3.rotation.x += 0.5;
-camera.position.z = 5; //moving camera initially at zero
+//camera.position.z = 15; //moving camera initially at zero
 
 function animate() {
   requestAnimationFrame(animate); //recursive call
