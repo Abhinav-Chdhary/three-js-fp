e0fdbcaefd04526ca354af0a517a7c21364b1fc4
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-11 18:16:44 +0530

    Update SomeEdGuy.js

--- SomeEdGuy.js
+++ SomeEdGuy.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import "./style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const sizes = {
   height: window.innerHeight,
@@ -16,31 +17,46 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.set(0, 0, 20);
 camera.lookAt(0, 0, 0);
+
 //renderer
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
 
 //light
-const light = new THREE.PointLight(0xffffff, 50, 100);
-light.position.set(0, 10, 10);
+const light = new THREE.PointLight(0xffffff, 100, 100);
+light.position.set(0, 0, 10);
 scene.add(light);
 
 //sphere
-const sphereGeometery = new THREE.SphereGeometry(3, 64, 64);
-const sphereMaterial = new THREE.MeshStandardMaterial({ color: "#00ff83" });
-const sphereMesh = new THREE.Mesh(sphereGeometery, sphereMaterial);
+const sphereGeometry = new THREE.SphereGeometry(3, 64, 64);
+const sphereMaterial = new THREE.MeshStandardMaterial({ color: "#00ff00" });
+const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphereMesh);
-
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-
-animate();
 
 //resize
 window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
+  //update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
 });
+
+//Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
